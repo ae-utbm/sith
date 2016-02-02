afc87888a652df5c4bd75d45c24f66ee54eb6901
Skia <lordbanana25@mailoo.org>
2016-02-02 11:00:08 +0100

    Finish the move to Jinja2

--- core/views/group.py
+++ core/views/group.py
@@ -10,11 +10,11 @@ class GroupListView(CanEditMixin, ListView):
     Displays the group list
     """
     model = Group
-    template_name = "core/group_list.html"
+    template_name = "core/group_list.jinja"
 
 class GroupEditView(CanEditMixin, UpdateView):
     model = Group
     pk_url_kwarg = "group_id"
-    template_name = "core/group_edit.html"
+    template_name = "core/group_edit.jinja"
     form_class = GroupEditForm
 
