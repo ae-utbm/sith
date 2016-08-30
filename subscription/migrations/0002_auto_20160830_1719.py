b21d9f6ff2919025c9c676508e6b4b8548889fc1
Skia <skia@libskia.so>
2016-08-30 17:33:45 +0200

    Add price for subscriptions

---
+++ subscription/migrations/0002_auto_20160830_1719.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('subscription', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='subscription',
+            name='location',
+            field=models.CharField(max_length=20, verbose_name='location', choices=[('BELFORT', 'Belfort'), ('SEVENANS', 'Sevenans'), ('MONTBELIARD', 'Montbéliard'), ('EBOUTIC', 'Eboutic')]),
+        ),
+    ]
