29fb0af893519d2c81e4551c0ae759bb24bac468
guillaume-renaud <renaudg779@gmail.com>
2016-11-08 18:14:41 +0100

    Addition of the stock parameter to the counter admin list

--- stock/urls.py
+++ stock/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import include, url
 from stock.views import *
 
 urlpatterns = [
-    url(r'^(?P<counter_id>[0-9]+)$', StockListView.as_view(), name='stock_list'),
-    url(r'^(?P<counter_id>[0-9]+)/new$', StockCreateView.as_view(), name='stock_new'),
+    url(r'^(?P<stock_id>[0-9]+)$', StockMain.as_view(), name='main'),
+    url(r'^new/counter/(?P<counter_id>[0-9]+)$', StockCreateView.as_view(), name='new'),
+    url(r'^(?P<stock_id>[0-9]+)/newItem$', StockItemCreateView.as_view(), name='new_item'),
 ]
--- stock/views.py
+++ stock/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, RedirectView, TemplateView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView, ProcessFormView, FormMixin
 
-# Create your views here.
+
+from stock.models import Stock
+
+class StockMain(DetailView):
+	"""
+		The stock view for the counter owner
+	"""
+	model = Stock
+	template_name = 'stock/stock_main.jinja'
+	pk_url_kwarg = "stock_id"
+
+class StockCreateView(CreateView):
+	"""
+		docstring for StockCreateView
+	"""
+
+class StockItemCreateView(CreateView):
+	"""
+	
+	"""
