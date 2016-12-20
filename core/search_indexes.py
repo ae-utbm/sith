bdd29a4d659c1fd8c73843acdaf9009ffe4a3f6d
Skia <skia@libskia.so>
2016-12-20 15:19:11 +0100

    Allow quick index updates

--- core/search_indexes.py
+++ core/search_indexes.py
@@ -13,3 +13,5 @@ class UserIndex(indexes.SearchIndex, indexes.Indexable):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.all()
 
+    def get_updated_field(self):
+        return "last_update"
