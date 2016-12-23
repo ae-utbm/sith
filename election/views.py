9bb5951dacd29f7bf547584bafabf88e91f89450
Jean-Baptiste Lenglet <jbaptiste.lenglet+git@gmail.com>
2016-12-23 01:32:42 +0100

    Removed request.user as user bc it already exists

--- election/views.py
+++ election/views.py
@@ -100,7 +100,6 @@ class ElectionDetailView(CanViewMixin, DetailView):
         kwargs = super(ElectionDetailView, self).get_context_data(**kwargs)
         kwargs['candidate_form'] = CandidateForm(self.get_object().id)
         kwargs['election_form'] = VoteForm(self.get_object(), self.request.user)
-        kwargs['user'] = self.request.user
         return kwargs
 
 
