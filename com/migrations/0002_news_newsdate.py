df7b0a5ffe792da420c1bc324abfc516bfb2939a
Skia <skia@libskia.so>
2016-12-24 02:12:46 +0100

    Clean migrations for news system

--- com/migrations/0002_news_newsdate.py
+++ com/migrations/0002_news_newsdate.py
@@ -8,8 +8,8 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('club', '0005_auto_20161120_1149'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('com', '0001_initial'),
     ]
 
@@ -17,24 +17,24 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='News',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
                 ('title', models.CharField(max_length=64, verbose_name='title')),
                 ('summary', models.TextField(verbose_name='summary')),
                 ('content', models.TextField(verbose_name='content')),
-                ('type', models.CharField(max_length=16, choices=[('NOTICE', 'Notice'), ('EVENT', 'Event'), ('WEEKLY', 'Weekly'), ('CALL', 'Call')], default='EVENT', verbose_name='type')),
+                ('type', models.CharField(choices=[('NOTICE', 'Notice'), ('EVENT', 'Event'), ('WEEKLY', 'Weekly'), ('CALL', 'Call')], default='EVENT', max_length=16, verbose_name='type')),
                 ('is_moderated', models.BooleanField(default=False, verbose_name='is moderated')),
-                ('club', models.ForeignKey(to='club.Club', verbose_name='club', related_name='news')),
-                ('moderator', models.ForeignKey(verbose_name='owner', to=settings.AUTH_USER_MODEL, related_name='moderated_news', null=True)),
-                ('owner', models.ForeignKey(to=settings.AUTH_USER_MODEL, verbose_name='owner', related_name='owned_news')),
+                ('author', models.ForeignKey(related_name='owned_news', to=settings.AUTH_USER_MODEL, verbose_name='author')),
+                ('club', models.ForeignKey(related_name='news', to='club.Club', verbose_name='club')),
+                ('moderator', models.ForeignKey(related_name='moderated_news', null=True, to=settings.AUTH_USER_MODEL, verbose_name='moderator')),
             ],
         ),
         migrations.CreateModel(
             name='NewsDate',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
                 ('start_date', models.DateTimeField(null=True, blank=True, verbose_name='start_date')),
                 ('end_date', models.DateTimeField(null=True, blank=True, verbose_name='end_date')),
-                ('news', models.ForeignKey(to='com.News', verbose_name='news_date', related_name='dates')),
+                ('news', models.ForeignKey(related_name='dates', to='com.News', verbose_name='news_date')),
             ],
         ),
     ]
