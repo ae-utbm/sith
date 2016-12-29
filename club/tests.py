0a9eb2fd3336d5b541972ff3cd81ce40de52d0ea
Skia <skia@libskia.so>
2016-12-29 01:30:37 +0100

    Fix club tests

--- club/tests.py
+++ club/tests.py
@@ -17,14 +17,20 @@ class ClubTest(TestCase):
 
     def test_create_add_user_to_club_from_root_ok(self):
         self.client.login(username='root', password='plop')
-        self.client.post(reverse("club:club_members", kwargs={"club_id":self.bdf.id}), {"user": self.skia.id, "role": 3})
+        self.client.post(reverse("club:club_members", kwargs={"club_id":self.bdf.id}), {
+            "user": self.skia.id,
+            "start_date": "12/06/2016",
+            "role": 3})
         response = self.client.get(reverse("club:club_members", kwargs={"club_id":self.bdf.id}))
         self.assertTrue(response.status_code == 200)
         self.assertTrue("S&#39; Kia</a></td>\\n                <td>Responsable info</td>" in str(response.content))
 
     def test_create_add_user_to_club_from_root_fail_not_subscriber(self):
         self.client.login(username='root', password='plop')
-        response = self.client.post(reverse("club:club_members", kwargs={"club_id":self.bdf.id}), {"user": self.guy.id, "role": 3})
+        response = self.client.post(reverse("club:club_members", kwargs={"club_id":self.bdf.id}), {
+            "user": self.guy.id,
+            "start_date": "12/06/2016",
+            "role": 3})
         self.assertTrue(response.status_code == 200)
         self.assertTrue('<ul class="errorlist nonfield"><li>' in str(response.content))
         response = self.client.get(reverse("club:club_members", kwargs={"club_id":self.bdf.id}))
@@ -32,27 +38,45 @@ class ClubTest(TestCase):
 
     def test_create_add_user_to_club_from_root_fail_already_in_club(self):
         self.client.login(username='root', password='plop')
-        self.client.post(reverse("club:club_members", kwargs={"club_id":self.bdf.id}), {"user": self.skia.id, "role": 3})
+        self.client.post(reverse("club:club_members", kwargs={"club_id":self.bdf.id}), {
+            "user": self.skia.id,
+            "start_date": "12/06/2016",
+            "role": 3})
         response = self.client.get(reverse("club:club_members", kwargs={"club_id":self.bdf.id}))
         self.assertTrue("S&#39; Kia</a></td>\\n                <td>Responsable info</td>" in str(response.content))
-        response = self.client.post(reverse("club:club_members", kwargs={"club_id":self.bdf.id}), {"user": self.skia.id, "role": 4})
+        response = self.client.post(reverse("club:club_members", kwargs={"club_id":self.bdf.id}), {
+            "user": self.skia.id,
+            "start_date": "12/06/2016",
+            "role": 4})
         self.assertTrue(response.status_code == 200)
         self.assertFalse("S&#39; Kia</a></td>\\n                <td>Secrétaire</td>" in str(response.content))
 
     def test_create_add_user_to_club_from_skia_ok(self):
         self.client.login(username='root', password='plop')
-        self.client.post(reverse("club:club_members", kwargs={"club_id":self.bdf.id}), {"user": self.skia.id, "role": 10})
+        self.client.post(reverse("club:club_members", kwargs={"club_id":self.bdf.id}), {
+            "user": self.skia.id,
+            "start_date": "12/06/2016",
+            "role": 10})
         self.client.login(username='skia', password='plop')
-        self.client.post(reverse("club:club_members", kwargs={"club_id":self.bdf.id}), {"user": self.rbatsbak.id, "role": 9})
+        self.client.post(reverse("club:club_members", kwargs={"club_id":self.bdf.id}), {
+            "user": self.rbatsbak.id,
+            "start_date": "12/06/2016",
+            "role": 9})
         response = self.client.get(reverse("club:club_members", kwargs={"club_id":self.bdf.id}))
         self.assertTrue(response.status_code == 200)
         self.assertTrue("""Richard Batsbak</a></td>\\n                <td>Vice-Pr\\xc3\\xa9sident</td>""" in str(response.content))
 
     def test_create_add_user_to_club_from_richard_fail(self):
         self.client.login(username='root', password='plop')
-        self.client.post(reverse("club:club_members", kwargs={"club_id":self.bdf.id}), {"user": self.rbatsbak.id, "role": 3})
+        self.client.post(reverse("club:club_members", kwargs={"club_id":self.bdf.id}), {
+            "user": self.rbatsbak.id,
+            "start_date": "12/06/2016",
+            "role": 3})
         self.client.login(username='rbatsbak', password='plop')
-        response = self.client.post(reverse("club:club_members", kwargs={"club_id":self.bdf.id}), {"user": self.skia.id, "role": 10})
+        response = self.client.post(reverse("club:club_members", kwargs={"club_id":self.bdf.id}), {
+            "user": self.skia.id,
+            "start_date": "12/06/2016",
+            "role": 10})
         self.assertTrue(response.status_code == 200)
         self.assertTrue("<li>Vous n&#39;avez pas la permission de faire cela</li>" in str(response.content))
 
