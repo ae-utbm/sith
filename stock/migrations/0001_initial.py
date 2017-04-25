e26ad857290f789e5795e01c843fbaee0ed63217
Skia <skia@libskia.so>
2017-04-25 09:49:05 +0200

    Reset stock migrations

--- stock/migrations/0001_initial.py
+++ stock/migrations/0001_initial.py
@@ -15,39 +15,56 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ShoppingList',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
                 ('date', models.DateTimeField(verbose_name='date')),
                 ('name', models.CharField(max_length=64, verbose_name='name')),
                 ('todo', models.BooleanField(verbose_name='todo')),
+                ('comment', models.TextField(verbose_name='comment', blank=True, null=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='ShoppingListItem',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('name', models.CharField(max_length=64, verbose_name='name')),
+                ('tobuy_quantity', models.IntegerField(verbose_name='quantity to buy', help_text='quantity to buy during the next shopping session', default=6)),
+                ('bought_quantity', models.IntegerField(verbose_name='quantity bought', help_text='quantity bought during the last shopping session', default=0)),
+                ('shopping_lists', models.ManyToManyField(verbose_name='shopping lists', related_name='shopping_items_to_buy', to='stock.ShoppingList')),
             ],
         ),
         migrations.CreateModel(
             name='Stock',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
                 ('name', models.CharField(max_length=64, verbose_name='name')),
-                ('counter', models.OneToOneField(to='counter.Counter', verbose_name='counter', related_name='stock')),
+                ('counter', models.OneToOneField(verbose_name='counter', related_name='stock', to='counter.Counter')),
             ],
         ),
         migrations.CreateModel(
             name='StockItem',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
                 ('name', models.CharField(max_length=64, verbose_name='name')),
-                ('unit_quantity', models.IntegerField(default=0, help_text='number of element in one box', verbose_name='unit quantity')),
-                ('effective_quantity', models.IntegerField(default=0, help_text='number of box', verbose_name='effective quantity')),
-                ('stock_owner', models.ForeignKey(to='stock.Stock', related_name='items')),
-                ('type', models.ForeignKey(to='counter.ProductType', on_delete=django.db.models.deletion.SET_NULL, null=True, verbose_name='type', blank=True, related_name='stock_items')),
+                ('unit_quantity', models.IntegerField(verbose_name='unit quantity', help_text='number of element in one box', default=0)),
+                ('effective_quantity', models.IntegerField(verbose_name='effective quantity', help_text='number of box', default=0)),
+                ('minimal_quantity', models.IntegerField(verbose_name='minimal quantity', help_text='if the effective quantity is less than the minimal, item is added to the shopping list', default=1)),
+                ('stock_owner', models.ForeignKey(related_name='items', to='stock.Stock')),
+                ('type', models.ForeignKey(blank=True, null=True, verbose_name='type', related_name='stock_items', on_delete=django.db.models.deletion.SET_NULL, to='counter.ProductType')),
             ],
         ),
         migrations.AddField(
-            model_name='shoppinglist',
-            name='items_to_buy',
-            field=models.ManyToManyField(to='stock.StockItem', related_name='shopping_lists', verbose_name='items to buy'),
+            model_name='shoppinglistitem',
+            name='stockitem_owner',
+            field=models.ForeignKey(null=True, related_name='shopping_item', to='stock.StockItem'),
+        ),
+        migrations.AddField(
+            model_name='shoppinglistitem',
+            name='type',
+            field=models.ForeignKey(blank=True, null=True, verbose_name='type', related_name='shoppinglist_items', on_delete=django.db.models.deletion.SET_NULL, to='counter.ProductType'),
         ),
         migrations.AddField(
             model_name='shoppinglist',
             name='stock_owner',
-            field=models.ForeignKey(to='stock.Stock', null=True, related_name='shopping_lists'),
+            field=models.ForeignKey(null=True, related_name='shopping_lists', to='stock.Stock'),
         ),
     ]
