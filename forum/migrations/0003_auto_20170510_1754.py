5060114305967117aea36cdb6ef9262c6c9c202e
Skia <skia@libskia.so>
2017-05-10 19:18:33 +0200

    Add missing forum migration (and tiny translation fix)

---
+++ forum/migrations/0003_auto_20170510_1754.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('forum', '0002_auto_20170312_1753'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='forum',
+            name='edit_groups',
+            field=models.ManyToManyField(blank=True, default=[4], related_name='editable_forums', to='core.Group'),
+        ),
+    ]
