c83a990165854af975a0527566af68ff104a7659
klmp200 <klmp200@klmp200.net>
2017-08-22 16:03:24 +0200

    Wtf migration blocking me

---
+++ counter/migrations/0015_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('counter', '0014_auto_20170817_1537'),
+        ('counter', '0014_auto_20170816_1521'),
+    ]
+
+    operations = [
+    ]
