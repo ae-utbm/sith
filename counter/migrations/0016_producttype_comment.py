6113de33ec2468936697d4a2969f528ae9b6941c
Krophil <pierre.brunet@krophil.fr>
2017-10-22 21:10:02 +0200

    Add comment in product type

---
+++ counter/migrations/0016_producttype_comment.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('counter', '0015_merge'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='producttype',
+            name='comment',
+            field=models.TextField(verbose_name='comment', blank=True, null=True),
+        ),
+    ]
