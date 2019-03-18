f9d4f41b7cb7ef1dbdccca7162ffa704e7672ac2
Bartuccio Antoine <klmp200@klmp200.net>
2019-03-18 20:52:35 +0100

    rootplace: create a cli utility to delete user's forum message to avoid connection timeout error

---
+++ rootplace/management/commands/delete_all_forum_user_messages.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*
+#
+# Copyright 2017
+# - Sli <antoine@bartuccio.fr>
+#
+# Ce fichier fait partie du site de l'Association des Étudiants de l'UTBM,
+# http://ae.utbm.fr.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License a published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Sofware Foundation, Inc., 59 Temple
+# Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+#
+
+from django.core.management.base import BaseCommand
+
+from core.models import User
+from rootplace.views import delete_all_forum_user_messages
+
+
+class Command(BaseCommand):
+    """
+        Delete all forum messages from an user
+        Messages are soft deleted and are still visible from admins
+    """
+
+    help = "Delete all user's forum message"
+
+    def add_arguments(self, parser):
+        parser.add_argument("user_id", type=int)
+
+    def handle(self, *args, **options):
+
+        user = User.objects.filter(id=options["user_id"]).first()
+
+        if user is None:
+            print("User with ID %s not found" % (options["user_id"],))
+            return
+
+        confirm = input(
+            "User selected %s\nDo you really want to delete all user's messages ? [y/N] "
+            % (user,)
+        )
+
+        if confirm != "y" and confirm != "Y":
+            print("Operation aborted")
+            return
+
+        delete_all_forum_user_messages(user, User.objects.get(id=0), True)
