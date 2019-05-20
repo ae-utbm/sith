3bddf176d8235191203cc257a73646cf34d3fa71
Bartuccio Antoine <klmp200@klmp200.net>
2019-05-20 19:12:53 +0200

    Fix typo for NFC cards

--- counter/views.py
+++ counter/views.py
@@ -115,7 +115,7 @@ class StudentCardForm(forms.ModelForm):
         cleaned_data = super(StudentCardForm, self).clean()
         uid = cleaned_data.get("uid", None)
         if not uid or not StudentCard.is_valid(uid):
-            raise forms.ValidationError(_("This uid is invalid"), code="invalid")
+            raise forms.ValidationError(_("This UID is invalid"), code="invalid")
         return cleaned_data
 
 
