ef40baaa845298fcd54b81b104afe64b106e1bd1
Bartuccio Antoine <klmp200@klmp200.net>
2019-08-10 23:17:00 +0200

    documentation: bump sphinx version

--- doc/conf.py
+++ doc/conf.py
@@ -66,6 +66,3 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-
-# Disable contents.rst for forward compatibility
-master_doc = "index"
