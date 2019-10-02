2da0560ec8644a88b5f947737f331db2a3e04ea3
Bartuccio Antoine <klmp200@klmp200.net>
2019-10-02 15:32:33 +0200

    com: fix error 500 when editing poster with com admin

--- com/views.py
+++ com/views.py
@@ -739,8 +739,7 @@ class PosterEditBaseView(UpdateView):
 
     def get_context_data(self, **kwargs):
         kwargs = super(PosterEditBaseView, self).get_context_data(**kwargs)
-        if not self.request.user.is_com_admin:
-            kwargs["club"] = self.club
+        kwargs["club"] = kwargs.get("club", self.club)
         return kwargs
 
     def form_valid(self, form):
