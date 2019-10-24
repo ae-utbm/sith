00c96f5b71d4255a0a3ef30ad0fa755ecef4e17f
tleb <theo.lebrun@live.fr>
2019-10-24 14:40:26 +0200

    eboutic: fix account amount

--- eboutic/views.py
+++ eboutic/views.py
@@ -107,7 +107,10 @@ class EbouticMain(TemplateView):
         kwargs["basket"] = self.basket
         kwargs["eboutic"] = Counter.objects.filter(type="EBOUTIC").first()
         kwargs["categories"] = ProductType.objects.all()
-        kwargs["customer_amount"] = self.request.user.customer.amount
+        if hasattr(self.request.user, "customer"):
+            kwargs["customer_amount"] = self.request.user.customer.amount
+        else:
+            kwargs["customer_amount"] = None
         if not self.request.user.was_subscribed:
             kwargs["categories"] = kwargs["categories"].exclude(
                 id=settings.SITH_PRODUCTTYPE_SUBSCRIPTION
@@ -151,7 +154,10 @@ class EbouticCommand(TemplateView):
 
     def get_context_data(self, **kwargs):
         kwargs = super(EbouticCommand, self).get_context_data(**kwargs)
-        kwargs["customer_amount"] = self.request.user.customer.amount
+        if hasattr(self.request.user, "customer"):
+            kwargs["customer_amount"] = self.request.user.customer.amount
+        else:
+            kwargs["customer_amount"] = None
         kwargs["et_request"] = OrderedDict()
         kwargs["et_request"]["PBX_SITE"] = settings.SITH_EBOUTIC_PBX_SITE
         kwargs["et_request"]["PBX_RANG"] = settings.SITH_EBOUTIC_PBX_RANG
