713cd921419c91395b92159e467a217c9d5063bb
lsacienne <alexandre.viala@outlook.com>
2022-05-21 12:23:23 +0200

    Modification of the settings to fit better with the code

--- sith/settings.py
+++ sith/settings.py
@@ -389,7 +389,7 @@ SITH_SUBSCRIPTION_LOCATIONS = [
 
 SITH_COUNTER_BARS = [(1, "MDE"), (2, "Foyer"), (35, "La Gommette")]
 
-SITH_COUNTER_OFFICES = [(17, "BdF"), (19, "AE")]
+SITH_COUNTER_OFFICES = {17: "BdF", 19: "AE"}
 
 SITH_COUNTER_PAYMENT_METHOD = [
     ("CHECK", _("Check")),
