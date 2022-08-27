cb6e037f5e990ce9c8c3bf96683bb4d4dcb55016
Théo DURR <git@theodurr.fr>
2022-08-27 21:52:16 +0200

    Fixed some mess done on settings.py

--- sith/settings.py
+++ sith/settings.py
@@ -680,6 +680,8 @@ if SENTRY_DSN:
     sentry_sdk.init(
         dsn=SENTRY_DSN,
         integrations=[DjangoIntegration()],
+        traces_sample_rate=1.0,
+        send_default_pii=True
         environment=SENTRY_ENV,
     )
 
