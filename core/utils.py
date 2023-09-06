ae-utbm/sith

commit 6c1fa6de0bd44a21646ecccf4ae703ddacdf0f43

remove-useless-queries-counter-stats (#519)

core/utils.py
@@ -31,7 +31,6 @@ from datetime import date
 
 from PIL import ExifTags
 
-# from exceptions import IOError
 import PIL
 
 from django.conf import settings
@@ -52,14 +51,12 @@ def get_start_of_semester(d=date.today()):
     year = today.year
     start = date(year, settings.SITH_START_DATE[0], settings.SITH_START_DATE[1])
     start2 = start.replace(month=(start.month + 6) % 12)
-    if start > start2:
-        start, start2 = start2, start
-    if today < start:
-        return start2.replace(year=year - 1)
-    elif today < start2:
-        return start
-    else:
-        return start2
+    spring, autumn = min(start, start2), max(start, start2)
+    if today > autumn:  # autumn semester
+        return autumn
+    if today > spring:  # spring semester
+        return spring
+    return autumn.replace(year=year - 1)  # autumn semester of last year
 
 
 def get_semester(d=date.today()):

core/static/core/style.scss
@@ -1198,18 +1198,38 @@ blockquote h5:first-child {
 }
 
 table {
-  width: 100%;
-  font-size: 0.9em;
+  width: 90%;
+  margin: 15px auto;
+  border-collapse: collapse;
+  border-spacing: 0;
+  border-radius: 5px;
+  -moz-border-radius: 5px;
+  overflow: hidden;
+  box-shadow: rgba(60, 64, 67, .3) 0 1px 3px 0, rgba(60, 64, 67, .15) 0 4px 8px 3px;
+}
+
+@media screen and (max-width: 500px){
+  table {
+    width: 100%;
+  }
 }
 
 th {
   padding: 4px;
 }
 
+td, th {
+  vertical-align: middle;
+  text-align: center;
+  padding: 5px 10px;
+  > ul {
+    margin-top: 0;
+  }
+}
+
 td {
   padding: 4px;
   margin: 5px;
-  border: solid 1px $primary-neutral-color;
   border-collapse: collapse;
   vertical-align: top;
   overflow: hidden;
@@ -1219,17 +1239,28 @@ td {
   }
 }
 
+th, thead td {
+  text-align: center;
+  border-top: none;
+}
+
 thead {
-  font-weight: bold;
+  background-color: #354a5f;
+  color: white;
 }
 
 tbody > tr {
   &:nth-child(even) {
     background: $primary-neutral-light-color;
   }
-  &:hover {
+  &.clickable:hover {
+    cursor: pointer;
     background: $secondary-neutral-light-color;
     width: 100%;
+  }
+  &.highlight {
+    color: $primary-dark-color;
+    font-style: italic;
   }
 }
 

core/templates/core/user_detail.jinja
@@ -5,8 +5,12 @@
 {% trans user_name=profile.get_display_name() %}{{ user_name }}'s profile{% endtrans %}
 {% endblock %}
 
+{% block additional_js %}
+    <script src="{{ static('core/js/alpinejs.min.js') }}" defer></script>
+{% endblock %}
+
 {% block content %}
-<div id="user_profile_page">
+<div id="user_profile_page" x-data>
     <div id="user_profile">
         <!-- Profile -->
         <div id="user_profile_infos">
@@ -15,7 +19,7 @@
                 <div id="user_profile_infos_nick">&laquo; {{ profile.nick_name }} &raquo;</div>
             {% endif %}
 
-            
+
 
             {% if profile.quote %}
                 <div id="user_profile_infos_quote">
@@ -146,24 +150,35 @@
         </div>
     {% endif %}
     {% if profile.was_subscribed and (user == profile or user.can_read_subscription_history)%}
-        <div id="drop_subscriptions">
-            <h5>{% trans %}Subscription history{% endtrans %}</h5>
-            <table>
-                <tr>
-                    <th>{% trans %}Subscription start{% endtrans %}</th>
-                    <th>{% trans %}Subscription end{% endtrans %}</th>
-                    <th>{% trans %}Subscription type{% endtrans %}</th>
-                    <th>{% trans %}Payment method{% endtrans %}</th>
-                </tr>
-            {% for sub in profile.subscriptions.all() %}
-                <tr>
-                    <td>{{ sub.subscription_start }}</td>
-                    <td>{{ sub.subscription_end }}</td>
-                    <td>{{ sub.subscription_type }}</td>
-                    <td>{{ sub.get_payment_method_display() }}</td>
-                </tr>
-            {% endfor %}
-            </table>
+        <div class="collapse" :class="{'shadow': collapsed}" x-data="{collapsed: false}" x-cloak>
+            <div class="collapse-header clickable" @click="collapsed = !collapsed">
+                <span class="collapse-header-text">
+                    {% trans %}Subscription history{% endtrans %}
+                </span>
+                <span class="collapse-header-icon" :class="{'reverse': collapsed}">
+                    <i class="fa fa-caret-down"></i>
+                </span>
+            </div>
+            <div class="collapse-body" x-show="collapsed" x-transition.scale.origin.top>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>{% trans %}Subscription start{% endtrans %}</th>
+                            <th>{% trans %}Subscription end{% endtrans %}</th>
+                            <th>{% trans %}Subscription type{% endtrans %}</th>
+                            <th>{% trans %}Payment method{% endtrans %}</th>
+                        </tr>
+                    </thead>
+                    {% for sub in profile.subscriptions.all() %}
+                    <tr>
+                        <td>{{ sub.subscription_start }}</td>
+                        <td>{{ sub.subscription_end }}</td>
+                        <td>{{ sub.subscription_type }}</td>
+                        <td>{{ sub.get_payment_method_display() }}</td>
+                    </tr>
+                    {% endfor %}
+                </table>
+            </div>
         </div>
     {% endif %}
 
@@ -177,13 +192,25 @@
             <input type="submit" value="{% trans %}Give gift{% endtrans %}">
         </form>
         {% if profile.gifts.exists() %}
-        <br>
-            <div id="drop_gifts">
-                <h5>{% trans %}Last given gift :{% endtrans %} {{ profile.gifts.order_by('-date').first() }}</h5>
-                <div>
+            {% set gifts = profile.gifts.order_by("-date")|list %}
+            <br>
+            <div class="collapse" :class="{'shadow': collapsed}" x-data="{collapsed: false}" x-cloak>
+                <div class="collapse-header clickable" @click="collapsed = !collapsed">
+                    <span class="collapse-header-text">
+                        {% trans %}Last given gift :{% endtrans %} {{ gifts[0] }}
+                    </span>
+                    <span class="collapse-header-icon" :class="{'reverse': collapsed}">
+                        <i class="fa fa-caret-down"></i>
+                    </span>
+                </div>
+                <div class="collapse-body" x-show="collapsed" x-transition.scale.origin.top>
                     <ul>
-                    {% for gift in profile.gifts.all().order_by('-date') %}
-                        <li>{{ gift }} <a href="{{ url('core:user_gift_delete', user_id=profile.id, gift_id=gift.id) }}">{% trans %}Delete{% endtrans %}</a></li>
+                    {% for gift in gifts %}
+                        <li>{{ gift }}
+                            <a href="{{ url('core:user_gift_delete', user_id=profile.id, gift_id=gift.id) }}">
+                                <i class="fa fa-trash"></i>
+                            </a>
+                        </li>
                     {% endfor %}
                     </ul>
                 </div>
@@ -224,13 +251,6 @@ $(function(){
 });
 $(function(){
     $("#drop_gifts").accordion({
-        heightStyle: "content",
-        collapsible: true,
-        active: false
-    });
-});
-$(function(){
-    $("#drop_subscriptions").accordion({
         heightStyle: "content",
         collapsible: true,
         active: false

core/templatetags/renderer.py
@@ -23,11 +23,13 @@
 #
 #
 
+import datetime
 import phonenumbers
 
 from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
+from django.utils.translation import ngettext
 from core.scss.processor import ScssProcessor
 
 from core.markdown import markdown as md
@@ -54,41 +56,26 @@ def phonenumber(value, country="FR", format=phonenumbers.PhoneNumberFormat.NATIO
         return value
 
 
-@register.filter()
-@stringfilter
-def datetime_format_python_to_PHP(python_format_string):
-    """
-    Given a python datetime format string, attempts to convert it to the nearest PHP datetime format string possible.
-    """
-    python2PHP = {
-        "%a": "D",
-        "%a": "D",
-        "%A": "l",
-        "%b": "M",
-        "%B": "F",
-        "%c": "",
-        "%d": "d",
-        "%H": "H",
-        "%I": "h",
-        "%j": "z",
-        "%m": "m",
-        "%M": "i",
-        "%p": "A",
-        "%S": "s",
-        "%U": "",
-        "%w": "w",
-        "%W": "W",
-        "%x": "",
-        "%X": "",
-        "%y": "y",
-        "%Y": "Y",
-        "%Z": "e",
-    }
+@register.filter(name="truncate_time")
+def truncate_time(value, time_unit):
+    value = str(value)
+    return {
+        "millis": lambda: value.split(".")[0],
+        "seconds": lambda: value.rsplit(":", maxsplit=1)[0],
+        "minutes": lambda: value.split(":", maxsplit=1)[0],
+        "hours": lambda: value.rsplit(" ")[0],
+    }[time_unit]()
 
-    php_format_string = python_format_string
-    for py, php in python2PHP.items():
-        php_format_string = php_format_string.replace(py, php)
-    return php_format_string
+
+@register.filter(name="format_timedelta")
+def format_timedelta(value: datetime.timedelta) -> str:
+    days = value.days
+    if days == 0:
+        return str(value)
+    remainder = value - datetime.timedelta(days=days)
+    return ngettext(
+        "%(nb_days)d day, %(remainder)s", "%(nb_days)d days, %(remainder)s", days
+    ) % {"nb_days": days, "remainder": str(remainder)}
 
 
 @register.simple_tag()

counter/models.py
@@ -22,13 +22,12 @@
 #
 #
 from __future__ import annotations
-from django.db.models import Sum, F
 
 from typing import Tuple
 
 from django.db import models
-from django.db.models import OuterRef, Exists
-from django.db.models.functions import Length
+from django.db.models import F, Value, Sum, QuerySet, OuterRef, Exists
+from django.db.models.functions import Concat, Length
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 from django.conf import settings
@@ -37,14 +36,14 @@ from django.core.validators import MinLengthValidator
 from django.forms import ValidationError
 from django.utils.functional import cached_property
 
-from datetime import timedelta, date
+from datetime import timedelta, date, datetime
 import random
 import string
 import os
 import base64
-import datetime
 from dict2xml import dict2xml
 
+from core.utils import get_start_of_semester
 from sith.settings import SITH_COUNTER_OFFICES, SITH_MAIN_CLUB
 from club.models import Club, Membership
 from accounting.models import CurrencyField
@@ -92,8 +91,9 @@ class Customer(models.Model):
         don't mix them) and a Product.
         """
         subscription = self.user.subscriptions.order_by("subscription_end").last()
-        time_diff = date.today() - subscription.subscription_end
-        return subscription is not None and time_diff < timedelta(days=90)
+        if subscription is None:
+            return False
+        return (date.today() - subscription.subscription_end) < timedelta(days=90)
 
     @classmethod
     def get_or_create(cls, user: User) -> Tuple[Customer, bool]:
@@ -491,7 +491,7 @@ class Counter(models.Model):
         """
         return self.is_open() and (
             (timezone.now() - self.permanencies.order_by("-activity").first().activity)
-            > datetime.timedelta(minutes=settings.SITH_COUNTER_MINUTE_INACTIVE)
+            > timedelta(minutes=settings.SITH_COUNTER_MINUTE_INACTIVE)
         )
 
     def barman_list(self):
@@ -516,6 +516,77 @@ class Counter(models.Model):
             if ae.get_membership_for(barman):
                 is_ae_member = True
         return is_ae_member
+
+    def get_top_barmen(self) -> QuerySet:
+        """
+        Return a QuerySet querying the office hours stats of all the barmen of all time
+        of this counter, ordered by descending number of hours.
+
+        Each element of the QuerySet corresponds to a barman and has the following data :
+            - the full name (first name + last name) of the barman
+            - the nickname of the barman
+            - the promo of the barman
+            - the total number of office hours the barman did attend
+        """
+        return (
+            self.permanencies.exclude(end=None)
+            .annotate(
+                name=Concat(F("user__first_name"), Value(" "), F("user__last_name"))
+            )
+            .annotate(nickname=F("user__nick_name"))
+            .annotate(promo=F("user__promo"))
+            .values("user", "name", "nickname", "promo")
+            .annotate(perm_sum=Sum(F("end") - F("start")))
+            .exclude(perm_sum=None)
+            .order_by("-perm_sum")
+        )
+
+    def get_top_customers(self, since=get_start_of_semester()) -> QuerySet:
+        """
+        Return a QuerySet querying the money spent by customers of this counter
+        since the specified date, ordered by descending amount of money spent.
+
+        Each element of the QuerySet corresponds to a customer and has the following data :
+            - the full name (first name + last name) of the customer
+            - the nickname of the customer
+            - the amount of money spent by the customer
+        """
+        return (
+            self.sellings.filter(date__gte=since)
+            .annotate(
+                name=Concat(
+                    F("customer__user__first_name"),
+                    Value(" "),
+                    F("customer__user__last_name"),
+                )
+            )
+            .annotate(nickname=F("customer__user__nick_name"))
+            .annotate(promo=F("customer__user__promo"))
+            .values("customer__user", "name", "nickname")
+            .annotate(
+                selling_sum=Sum(
+                    F("unit_price") * F("quantity"), output_field=CurrencyField()
+                )
+            )
+            .filter(selling_sum__gt=0)
+            .order_by("-selling_sum")
+        )
+
+    def get_total_sales(self, since=get_start_of_semester()) -> CurrencyField:
+        """
+        Compute and return the total turnover of this counter
+        since the date specified in parameter (by default, since the start of the current
+        semester)
+        :param since: timestamp from which to perform the calculation
+        :type since: datetime | date
+        :return: Total revenue earned at this counter
+        """
+        if isinstance(since, date):
+            since = datetime.combine(since, datetime.min.time())
+        total = self.sellings.filter(date__gte=since).aggregate(
+            total=Sum(F("quantity") * F("unit_price"), output_field=CurrencyField())
+        )["total"]
+        return total if total is not None else CurrencyField(0)
 
 
 class Refilling(models.Model):

counter/templates/counter/stats.jinja
@@ -2,43 +2,34 @@
 {% from 'core/macros.jinja' import user_profile_link %}
 
 {% block title %}
-{% trans counter_name=counter %}{{ counter_name }} stats{% endtrans %}
+    {% trans counter_name=counter %}{{ counter_name }} stats{% endtrans %}
+{% endblock %}
+
+{% block jquery_css %}
+    {# Remove jquery_css #}
 {% endblock %}
 
 {% block content %}
-<h3>{% trans counter_name=counter %}{{ counter_name }} stats{% endtrans %}</h3>
+    <h3>{% trans counter_name=counter %}{{ counter_name }} stats{% endtrans %}</h3>
     <h4>{% trans counter_name=counter.name %}Top 100 {{ counter_name }}{% endtrans %}</h4>
     <table>
         <thead>
-            <tr>
-                <td>{% trans %}Nb{% endtrans %}</td>
-                <td>{% trans %}User{% endtrans %}</td>
-                <td>{% trans %}Promo{% endtrans %}</td>
-                <td>{% trans %}Clubs{% endtrans %}</td>
-                <td>{% trans %}Total{% endtrans %}</td>
-                <td>{% trans %}Percentage{% endtrans %}</td>
-            </tr>
+        <tr>
+            <td>N°</td>
+            <td>{% trans %}User{% endtrans %}</td>
+            <td>{% trans %}Promo{% endtrans %}</td>
+            <td>{% trans %}Total{% endtrans %}</td>
+            <td>{% trans %}Percentage{% endtrans %}</td>
+        </tr>
         </thead>
         <tbody>
-        {% for r in top %}
-            {% set customer=Customer.objects.filter(user__id=r.customer__user).first() %}
-            {% if customer.user == user %}
-            <tr class="highlight">
-            {% else %}
-            <tr>
-            {% endif %}
+        {% for customer in top_customers %}
+            <tr class="{% if customer.user == request.user.id %}highlight{% endif %}">
                 <td>{{ loop.index }}</td>
-                <td>{{ customer.user.get_display_name() }}</td>
-                <td>{{ customer.user.promo or '' }}</td>
-                <td>
-                {% for m in customer.user.memberships.filter(club__parent=None, end_date=None,
-                        role__gt=settings.SITH_MAXIMUM_FREE_ROLE).all() -%}
-                    {%- if loop.index>1 -%}, {% endif -%}
-                    {{ m.club.name }}
-                {%- endfor %}
-                </td>
-                <td>{{ r.selling_sum }} €</td>
-                <td>{{ '%.2f'|format(100 * r.selling_sum / total_sellings) }}</td>
+                <td>{{ customer.name }} {% if customer.nickname %} ({{ customer.nickname }}) {% endif %}</td>
+                <td>{{ customer.promo or '' }}</td>
+                <td>{{ "%.2f"|format(customer.selling_sum) }} €</td>
+                <td>{{ '%.2f'|format(100 * customer.selling_sum / total_sellings) }}%</td>
             </tr>
         {% endfor %}
         </tbody>
@@ -47,23 +38,20 @@
     <h4>{% trans counter_name=counter.name %}Top 100 barman {{ counter_name }}{% endtrans %}</h4>
     <table>
         <thead>
-            <tr>
-                <td>{% trans %}Nb{% endtrans %}</td>
-                <td>{% trans %}User{% endtrans %}</td>
-                <td>{% trans %}Time{% endtrans %}</td>
-            </tr>
+        <tr>
+            <td>N°</td>
+            <td>{% trans %}User{% endtrans %}</td>
+            <td>{% trans %}Promo{% endtrans %}</td>
+            <td>{% trans %}Time{% endtrans %}</td>
+        </tr>
         </thead>
         <tbody>
-        {% for r in top_barman_semester %}
-            {% set u=User.objects.filter(id=r.user).first() %}
-            {% if u == user %}
-            <tr class="highlight">
-            {% else %}
-            <tr>
-            {% endif %}
+        {% for barman in top_barman_semester %}
+            <tr {% if barman.user == request.user.id %}class="highlight"{% endif %}>
                 <td>{{ loop.index }}</td>
-                <td>{{ u.get_display_name() }}</td>
-                <td>{{ r.perm_sum }}</td>
+                <td>{{ barman.name }} {% if barman.nickname %}({{ barman.nickname }}){% endif %}</td>
+                <td>{{ barman.promo or '' }}</td>
+                <td>{{ barman.perm_sum|format_timedelta|truncate_time("millis") }}</td>
             </tr>
         {% endfor %}
         </tbody>
@@ -72,23 +60,20 @@
     <h4>{% trans counter_name=counter.name %}Top 100 barman {{ counter_name }} (all semesters){% endtrans %}</h4>
     <table>
         <thead>
-            <tr>
-                <td>{% trans %}Nb{% endtrans %}</td>
-                <td>{% trans %}User{% endtrans %}</td>
-                <td>{% trans %}Time{% endtrans %}</td>
-            </tr>
+        <tr>
+            <td>N°</td>
+            <td>{% trans %}User{% endtrans %}</td>
+            <td>{% trans %}Promo{% endtrans %}</td>
+            <td>{% trans %}Time{% endtrans %}</td>
+        </tr>
         </thead>
         <tbody>
-        {% for r in top_barman %}
-            {% set u=User.objects.filter(id=r.user).first() %}
-            {% if u == user %}
-            <tr class="highlight">
-            {% else %}
-            <tr>
-            {% endif %}
+        {% for barman in top_barman %}
+            <tr {% if barman.user == request.user.id %}class="highlight"{% endif %}>
                 <td>{{ loop.index }}</td>
-                <td>{{ u.get_display_name() }}</td>
-                <td>{{ r.perm_sum }}</td>
+                <td>{{ barman.name }} {% if barman.nickname %}({{ barman.nickname }}){% endif %}</td>
+                <td>{{ barman.promo or '' }}</td>
+                <td>{{ barman.perm_sum|format_timedelta|truncate_time("millis") }}</td>
             </tr>
         {% endfor %}
         </tbody>

commit 0aa6ad841bb335e21c86394556a60f964143c93d

Fix bug introduced in #519

core/utils.py
@@ -44,7 +44,7 @@ def get_git_revision_short_hash() -> str:
         return ""
 
 
-def get_start_of_semester(d=date.today()):
+def get_start_of_semester(today=date.today()):
     """
     This function computes the start date of the semester with respect to the given date (default is today),
     and the start date given in settings.SITH_START_DATE.
@@ -54,16 +54,21 @@ def get_start_of_semester(d=date.today()):
         2015-03-17 -> 2015-02-15
         2015-01-11 -> 2014-08-15
     """
-    today = d
     year = today.year
-    start = date(year, settings.SITH_START_DATE[0], settings.SITH_START_DATE[1])
-    start2 = start.replace(month=(start.month + 6) % 12)
-    spring, autumn = min(start, start2), max(start, start2)
-    if today > autumn:  # autumn semester
-        return autumn
-    if today > spring:  # spring semester
-        return spring
-    return autumn.replace(year=year - 1)  # autumn semester of last year
+
+    # Get the start date of the autumn semester based on settings.SITH_START_DATE
+    autumn_start = date(year, settings.SITH_START_DATE[0], settings.SITH_START_DATE[1])
+    # Get the start date of the spring semester, 6 months from autumn_start
+    spring_start = autumn_start.replace(month=(autumn_start.month + 6) % 12)
+
+    # Ensure that spring_start represents the earlier semester start date
+    if autumn_start > spring_start:
+        spring_start, autumn_start = autumn_start, spring_start
+
+    # Determine the appropriate semester start date based on the current date
+    # If today is earlier than spring_start, return the autumn semester start date of the previous year
+    # Otherwise, return the spring semester start date of the current year
+    return autumn_start.replace(year=year - 1) if today < spring_start else spring_start
 
 
 def get_semester(d=date.today()):