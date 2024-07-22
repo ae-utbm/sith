58ff5b934acdda4d913b11244fd2693d456b30a4
Sli <antoine@bartuccio.fr>
2024-07-22 09:49:08 +0200

    add get_client_ip util function

--- sith/honeypot.py
+++ sith/honeypot.py
@@ -4,12 +4,14 @@ from typing import Any
 
 from django.http import HttpRequest, HttpResponse
 
+from core.utils import get_client_ip
+
 
 def custom_honeypot_error(
     request: HttpRequest, context: dict[str, Any]
 ) -> HttpResponse:
     logging.warning(
         f"[{strftime('%c', localtime())}] "
-        f"HoneyPot blocked user with ip {request.META.get('X-Forwarded-For')}"
+        f"HoneyPot blocked user with ip {get_client_ip(request)}"
     )
     return HttpResponse("Upon reading this, the http client was enlightened.")
