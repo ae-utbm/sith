7c759424839f3b7f24fef8dd4f610be5df31646d
thomas girod <thgirod@hotmail.com>
2024-09-20 14:08:29 +0200

    fix: profile picture deletion by board members

--- core/baker_recipes.py
+++ core/baker_recipes.py
@@ -1,9 +1,11 @@
 from datetime import timedelta
 
+from django.conf import settings
 from django.utils.timezone import now
 from model_bakery import seq
 from model_bakery.recipe import Recipe, related
 
+from club.models import Membership
 from core.models import User
 from subscription.models import Subscription
 
@@ -24,9 +26,27 @@ subscriber_user = Recipe(
     last_name=seq("user "),
     subscriptions=related(active_subscription),
 )
+"""A user with an active subscription."""
+
 old_subscriber_user = Recipe(
     User,
     first_name="old subscriber",
     last_name=seq("user "),
     subscriptions=related(ended_subscription),
 )
+"""A user with an ended subscription."""
+
+ae_board_membership = Recipe(
+    Membership,
+    start_date=now() - timedelta(days=30),
+    club_id=settings.SITH_MAIN_CLUB_ID,
+    role=settings.SITH_CLUB_ROLES_ID["Board member"],
+)
+
+board_user = Recipe(
+    User,
+    first_name="AE",
+    last_name=seq("member "),
+    memberships=related(ae_board_membership),
+)
+"""A user which is in the board of the AE."""
