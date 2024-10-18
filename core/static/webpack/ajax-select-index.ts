56cc4776a6490b611da08294c55ccf79be7a8afc
Sli <antoine@bartuccio.fr>
2024-10-18 23:26:04 +0200

    Create base class for ajax-select

--- core/static/webpack/ajax-select-index.ts
+++ core/static/webpack/ajax-select-index.ts
@@ -1,41 +1,89 @@
 import "tom-select/dist/css/tom-select.css";
 import { inheritHtmlElement, registerComponent } from "#core:utils/web-components";
 import TomSelect from "tom-select";
-import type { TomItem, TomLoadCallback, TomOption } from "tom-select/dist/types/types";
+import type {
+  RecursivePartial,
+  TomItem,
+  TomLoadCallback,
+  TomOption,
+  TomSettings,
+} from "tom-select/dist/types/types";
 import type { escape_html } from "tom-select/dist/types/utils";
 import { type UserProfileSchema, userSearchUsers } from "#openapi";
 
-@registerComponent("ajax-select")
-export class AjaxSelect extends inheritHtmlElement("select") {
+abstract class AjaxSelectBase extends inheritHtmlElement("select") {
+  static observedAttributes = ["delay", "placeholder", "max"];
   public widget: TomSelect;
-  public filter?: <T>(items: T[]) => T[];
+
+  private delay: number | null = null;
+  private placeholder = "";
+  private max: number | null = null;
+
+  attributeChangedCallback(name: string, _oldValue?: string, newValue?: string) {
+    switch (name) {
+      case "delay": {
+        this.delay = Number.parseInt(newValue) ?? null;
+        break;
+      }
+      case "placeholder": {
+        this.placeholder = newValue ?? "";
+        break;
+      }
+      case "max": {
+        this.max = Number.parseInt(newValue) ?? null;
+        break;
+      }
+      default: {
+        return;
+      }
+    }
+  }
 
   constructor() {
     super();
 
     window.addEventListener("DOMContentLoaded", () => {
-      this.loadTomSelect();
+      this.configureTomSelect(this.defaultTomSelectSettings());
+      this.setDefaultTomSelectBehaviors();
     });
   }
 
-  loadTomSelect() {
+  private defaultTomSelectSettings(): RecursivePartial<TomSettings> {
+    return {
+      maxItems: this.max,
+      loadThrottle: this.delay,
+      placeholder: this.placeholder,
+    };
+  }
+
+  private setDefaultTomSelectBehaviors() {
+    // Allow removing selected items by clicking on them
+    this.widget.on("item_select", (item: TomItem) => {
+      this.widget.removeItem(item);
+    });
+    // Remove typed text once an item has been selected
+    this.widget.on("item_add", () => {
+      this.widget.setTextboxValue("");
+    });
+  }
+
+  abstract configureTomSelect(defaultSettings: RecursivePartial<TomSettings>): void;
+}
+
+@registerComponent("user-ajax-select")
+export class UserAjaxSelect extends AjaxSelectBase {
+  public filter?: <T>(items: T[]) => T[];
+
+  configureTomSelect(defaultSettings: RecursivePartial<TomSettings>) {
     const minCharNumberForSearch = 2;
-    let maxItems = 1;
-
-    if (this.node.multiple) {
-      maxItems = Number.parseInt(this.node.dataset.max) ?? null;
-    }
-
     this.widget = new TomSelect(this.node, {
+      ...defaultSettings,
       hideSelected: true,
       diacritics: true,
       duplicates: false,
-      maxItems: maxItems,
-      loadThrottle: Number.parseInt(this.node.dataset.delay) ?? null,
       valueField: "id",
       labelField: "display_name",
-      searchField: ["display_name", "nick_name", "first_name", "last_name"],
-      placeholder: this.node.dataset.placeholder ?? "",
+      searchField: [], // Disable local search filter and rely on tested backend
       shouldLoad: (query: string) => {
         return query.length >= minCharNumberForSearch; // Avoid launching search with less than 2 characters
       },
@@ -79,15 +127,6 @@ export class AjaxSelect extends inheritHtmlElement("select") {
           return `<div class="no-results">${gettext("No results found")}</div>`;
         },
       },
-    });
-
-    // Allow removing selected items by clicking on them
-    this.widget.on("item_select", (item: TomItem) => {
-      this.widget.removeItem(item);
-    });
-    // Remove typed text once an item has been selected
-    this.widget.on("item_add", () => {
-      this.widget.setTextboxValue("");
     });
   }
 }
