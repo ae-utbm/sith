d70399e7cc51d1028d44f9e89ad1447de758f738
Sli <antoine@bartuccio.fr>
2025-02-17 15:58:53 +0100

    Integrate automatic redis startup with the project

---
+++ sith/pytest.py
@@ -0,0 +1,23 @@
+import pytest
+
+from processes.composer import start_composer, stop_composer
+
+from .environ import env
+
+# pytest-django uses the load_initial_conftest hook
+# it's the first hook loaded by pytest and can only
+# be defined in a proper pytest plugin
+# To use the composer before pytest-django loads
+# we need to define this hook and thus create a proper
+# pytest plugin. We can't just use conftest.py
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_load_initial_conftests(early_config, parser, args):
+    """Hook that loads the composer before the pytest-django plugin"""
+    if (procfile := env.str("PROCFILE_PYTEST", None)) is not None:
+        start_composer(procfile)
+
+
+def pytest_unconfigure(config):
+    stop_composer()
--- staticfiles/management/commands/runserver.py
+++ staticfiles/management/commands/runserver.py
@@ -1,13 +1,6 @@
-import logging
-import os
-import subprocess
-import sys
-
-from django.conf import settings
 from django.contrib.staticfiles.management.commands.runserver import (
     Command as Runserver,
 )
-from django.utils.autoreload import DJANGO_AUTORELOAD_ENV
 
 from staticfiles.processors import OpenApi
 
@@ -16,19 +9,5 @@ class Command(Runserver):
     """Light wrapper around default runserver that integrates javascirpt auto bundling."""
 
     def run(self, **options):
-        # OpenApi generation needs to be before the bundler
         OpenApi.compile()
-        # Run all other web processes but only if debug mode is enabled
-        # Also protects from re-launching the server if django reloads it
-        if os.environ.get(DJANGO_AUTORELOAD_ENV) is None:
-            logging.getLogger("django").info("Running complementary processes")
-            with subprocess.Popen(
-                [sys.executable, "-m", "honcho", "start"],
-                env={
-                    **os.environ,
-                    **{"BUNDLER_MODE": "serve" if settings.DEBUG else "compile"},
-                },
-            ):
-                super().run(**options)
-                return
         super().run(**options)
