62246f342d225af58fcd83f0f85e1a3777b447ae
Kenneth SOARES <kensoares@gmail.com>
2025-03-05 20:19:11 +0100

    removed unnecessary event listener

--- core/static/bundled/country-flags-index.ts
+++ core/static/bundled/country-flags-index.ts
@@ -1,5 +1,3 @@
 import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
 
-window.addEventListener("DOMContentLoaded", () => {
-  polyfillCountryFlagEmojis();
-});
+polyfillCountryFlagEmojis();
