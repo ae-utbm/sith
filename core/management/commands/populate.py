da700a1c2c407e99f61d7d241409e7b056c30a64
Thomas Girod <thgirod@hotmail.com>
2025-04-13 15:09:42 +0200

    create reservation models

--- core/management/commands/populate.py
+++ core/management/commands/populate.py
@@ -788,7 +788,16 @@ class Command(BaseCommand):
 
         subscribers = Group.objects.create(name="Subscribers")
         subscribers.permissions.add(
-            *list(perms.filter(codename__in=["add_news", "add_uvcomment"]))
+            *list(
+                perms.filter(
+                    codename__in=[
+                        "add_news",
+                        "add_uvcomment",
+                        "add_reservationslot",
+                        "view_reservationslot",
+                    ]
+                )
+            )
         )
         old_subscribers = Group.objects.create(name="Old subscribers")
         old_subscribers.permissions.add(
--- sith/settings.py
+++ sith/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     "trombi",
     "matmat",
     "pedagogy",
+    "reservation",
     "galaxy",
     "antispam",
     "api",
