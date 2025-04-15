2ae9baa82f47d770338f052ed7cd2ee0b58b035f
Sli <antoine@bartuccio.fr>
2025-04-15 18:50:25 +0200

    Fix etransaction_data endpoint

--- eboutic/static/bundled/eboutic/checkout-index.ts
+++ eboutic/static/bundled/eboutic/checkout-index.ts
@@ -1,13 +1,18 @@
 import { etransactioninfoFetchEtransactionData } from "#openapi";
 
 document.addEventListener("alpine:init", () => {
-  Alpine.data("etransaction", (initialData) => ({
+  Alpine.data("etransaction", (initialData, basketId: number) => ({
     data: initialData,
     isCbAvailable: Object.keys(initialData).length > 0,
 
     async fill() {
       this.isCbAvailable = false;
-      const res = await etransactioninfoFetchEtransactionData();
+      const res = await etransactioninfoFetchEtransactionData({
+        path: {
+          // biome-ignore lint/style/useNamingConvention: api is in snake_case
+          basket_id: basketId,
+        },
+      });
       if (res.response.ok) {
         this.data = res.data;
         this.isCbAvailable = true;
