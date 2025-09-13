c1a69246e8adbe9eeb6fa42d08a2cf5aa2aa01a0
imperosol <thgirod@hotmail.com>
2025-09-13 18:37:53 +0200

    Better management of roles in ClubMemberForm

--- club/views.py
+++ club/views.py
@@ -72,7 +72,7 @@ from core.auth.mixins import (
 )
 from core.models import PageRev
 from core.views import DetailFormView, PageEditViewBase, UseFragmentsMixin
-from core.views.mixins import FragmentMixin, TabedViewMixin
+from core.views.mixins import FragmentMixin, FragmentRenderer, TabedViewMixin
 from counter.models import Selling
 
 
@@ -307,8 +307,13 @@ class ClubMembersView(
     form_class = ClubOldMemberForm
     template_name = "club/club_members.jinja"
     current_tab = "members"
-    fragments = {"add_member_fragment": ClubAddMembersFragment}
     permission_required = "club.view_club"
+
+    def get_fragments(self) -> dict[str, type[FragmentMixin] | FragmentRenderer]:
+        membership = self.object.get_membership_for(self.request.user)
+        if membership and membership.role <= settings.SITH_MAXIMUM_FREE_ROLE:
+            return {}
+        return {"add_member_fragment": ClubAddMembersFragment}
 
     def get_fragment_data(self) -> dict[str, Any]:
         return {"add_member_fragment": {"club": self.object}}
