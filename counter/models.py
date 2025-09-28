c0a31f6e1350858eb58d26ab9afbb712c468a03a
imperosol <thgirod@hotmail.com>
2025-09-28 12:37:52 +0200

    adapt club members pages to new club roles framework

--- counter/models.py
+++ counter/models.py
@@ -600,7 +600,7 @@ class Counter(models.Model):
         if user.is_anonymous:
             return False
         mem = self.club.get_membership_for(user)
-        if mem and mem.role >= settings.SITH_CLUB_ROLES_ID["Treasurer"]:
+        if mem and mem.role.is_presidency:
             return True
         return user.is_in_group(pk=settings.SITH_GROUP_COUNTER_ADMIN_ID)
 
