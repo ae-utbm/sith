76eebaf54e2e12a772a091d95e021a0d29bfe733
Sli <antoine@bartuccio.fr>
2025-10-18 14:35:08 +0200

    Rename notification plugin import on alpine-index

--- core/static/bundled/alpine-index.js
+++ core/static/bundled/alpine-index.js
@@ -1,9 +1,9 @@
-import { alpinePlugin } from "#core:utils/notifications";
+import { alpinePlugin as notificationPlugin } from "#core:utils/notifications";
 import sort from "@alpinejs/sort";
 import Alpine from "alpinejs";
 
 Alpine.plugin(sort);
-Alpine.magic("notifications", alpinePlugin);
+Alpine.magic("notifications", notificationPlugin);
 window.Alpine = Alpine;
 
 window.addEventListener("DOMContentLoaded", () => {
