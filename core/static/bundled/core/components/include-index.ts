3bc4f1300eb823607772beeb5a7ac52da4bd3d7d
Sli <antoine@bartuccio.fr>
2026-03-26 23:48:38 +0100

    Fix link-once and script-once not triggering when another one disappears

--- core/static/bundled/core/components/include-index.ts
+++ core/static/bundled/core/components/include-index.ts
@@ -6,12 +6,34 @@ import { inheritHtmlElement, registerComponent } from "#core:utils/web-component
  **/
 @registerComponent("link-once")
 export class LinkOnce extends inheritHtmlElement("link") {
-  connectedCallback() {
-    super.connectedCallback(false);
+  refresh() {
+    this.clearNode();
+
     // We get href from node.attributes instead of node.href to avoid getting the domain part
     const href = this.node.attributes.getNamedItem("href").nodeValue;
     if (document.querySelectorAll(`link[href='${href}']`).length === 0) {
       this.appendChild(this.node);
+    }
+  }
+
+  clearNode() {
+    while (this.firstChild) {
+      this.removeChild(this.lastChild);
+    }
+  }
+
+  connectedCallback() {
+    super.connectedCallback(false);
+    this.refresh();
+  }
+
+  disconnectedCallback() {
+    this.clearNode();
+
+    // This re-triggers link-once elements that still exists and suppressed
+    // themeselves once it gets removed from the page
+    for (const link of document.getElementsByTagName("link-once")) {
+      (link as LinkOnce).refresh();
     }
   }
 }
@@ -22,12 +44,34 @@ export class LinkOnce extends inheritHtmlElement("link") {
  **/
 @registerComponent("script-once")
 export class ScriptOnce extends inheritHtmlElement("script") {
-  connectedCallback() {
-    super.connectedCallback(false);
+  refresh() {
+    this.clearNode();
+
     // We get src from node.attributes instead of node.src to avoid getting the domain part
     const src = this.node.attributes.getNamedItem("src").nodeValue;
     if (document.querySelectorAll(`script[src='${src}']`).length === 0) {
       this.appendChild(this.node);
+    }
+  }
+
+  clearNode() {
+    while (this.firstChild) {
+      this.removeChild(this.lastChild);
+    }
+  }
+
+  connectedCallback() {
+    super.connectedCallback(false);
+    this.refresh();
+  }
+
+  disconnectedCallback() {
+    this.clearNode();
+
+    // This re-triggers script-once elements that still exists and suppressed
+    // themeselves once it gets removed from the page
+    for (const link of document.getElementsByTagName("script-once")) {
+      (link as LinkOnce).refresh();
     }
   }
 }
