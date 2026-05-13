51c8e1e3dc581d0adcd349abad9a6e2b485bf429
imperosol <thgirod@hotmail.com>
2026-05-13 11:09:25 +0200

    update django-debug-toolbar panels

--- sith/settings.py
+++ sith/settings.py
@@ -636,16 +636,17 @@ if DEBUG:
     INSTALLED_APPS += ("debug_toolbar",)
     MIDDLEWARE = ("debug_toolbar.middleware.DebugToolbarMiddleware", *MIDDLEWARE)
     DEBUG_TOOLBAR_PANELS = [
-        "debug_toolbar.panels.versions.VersionsPanel",
+        "debug_toolbar.panels.history.HistoryPanel",
         "debug_toolbar.panels.timer.TimerPanel",
         "debug_toolbar.panels.settings.SettingsPanel",
         "debug_toolbar.panels.headers.HeadersPanel",
         "debug_toolbar.panels.request.RequestPanel",
         "debug_toolbar.panels.sql.SQLPanel",
-        "sith.toolbar_debug.TemplatesPanel",
+        "debug_toolbar.panels.templates.TemplatesPanel",
         "debug_toolbar.panels.cache.CachePanel",
         "debug_toolbar.panels.signals.SignalsPanel",
-        "debug_toolbar.panels.redirects.RedirectsPanel",
+        "debug_toolbar.panels.community.CommunityPanel",
+        "debug_toolbar.panels.profiling.ProfilingPanel",
     ]
     if not TESTING:
         SENTRY_ENV = "development"  # We can't test if it gets overridden in settings
