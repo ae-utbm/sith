ae-utbm/sith3

force CGU/EULA approval on account creation

core/migrations/0051_user_eula_approved.py
@@ -1,0 +1,15 @@
+# Generated by Django 5.2.17 on 2026-09-15 11:02
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [("core", "0050_alter_sithfile_moderator")]
+
+    operations = [
+        migrations.AddField(
+            model_name="user",
+            name="eula_approved",
+            field=models.BooleanField(default=False, verbose_name="EULA approved"),
+        ),
+    ]

core/models.py
@@ -291,6 +291,7 @@ class User(AbstractUser):
         ),
         blank=True,
     )
+    eula_approved = models.BooleanField(_("EULA approved"), default=False)
     godfathers = models.ManyToManyField("User", related_name="godchildren", blank=True)
 
     objects = CustomUserManager()

core/views/forms.py
@@ -42,6 +42,8 @@ from django.forms import (
     TextInput,
     Widget,
 )
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from django.utils.timezone import now
 from django.utils.translation import gettext_lazy as _
 from phonenumber_field.widgets import RegionalPhoneNumberWidget
@@ -146,8 +148,24 @@ class RegisteringForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ("first_name", "last_name", "email")
+        fields = ("first_name", "last_name", "email", "eula_approved")
         field_classes = {"email": AntiSpamEmailField}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["eula_approved"].required = True
+        self.fields["eula_approved"].label_suffix = ""
+        self.fields["eula_approved"].label = mark_safe(
+            _(
+                "I have read and I approve the "
+                '<a href="%(url)s" target="_blank">End User License Agreement</a>'
+            )
+            % {
+                "url": reverse(
+                    "core:download", kwargs={"file_id": settings.SITH_CGU_FILE_ID}
+                )
+            }
+        )
 
 
 class UserProfileForm(forms.ModelForm):

ae-utbm/sith

core/migrations/0051_user_eula_approved.py
@@ -1,0 +1,15 @@
+# Generated by Django 5.2.17 on 2026-09-15 11:02
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [("core", "0050_alter_sithfile_moderator")]
+
+    operations = [
+        migrations.AddField(
+            model_name="user",
+            name="cgu_approved",
+            field=models.BooleanField(default=False, verbose_name="EULA approved"),
+        ),
+    ]

core/models.py
@@ -291,6 +291,7 @@ class User(AbstractUser):
         ),
         blank=True,
     )
+    cgu_approved = models.BooleanField(_("EULA approved"), default=False)
     godfathers = models.ManyToManyField("User", related_name="godchildren", blank=True)
 
     objects = CustomUserManager()

core/views/forms.py
@@ -42,6 +42,8 @@ from django.forms import (
     TextInput,
     Widget,
 )
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from django.utils.timezone import now
 from django.utils.translation import gettext_lazy as _
 from phonenumber_field.widgets import RegionalPhoneNumberWidget
@@ -146,8 +148,24 @@ class RegisteringForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ("first_name", "last_name", "email")
+        fields = ("first_name", "last_name", "email", "cgu_approved")
         field_classes = {"email": AntiSpamEmailField}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["cgu_approved"].required = True
+        self.fields["cgu_approved"].label_suffix = ""
+        self.fields["cgu_approved"].label = mark_safe(
+            _(
+                "I have read and I approve the "
+                '<a href="%(url)s" target="_blank">End User License Agreement</a>'
+            )
+            % {
+                "url": reverse(
+                    "core:download", kwargs={"file_id": settings.SITH_CGU_FILE_ID}
+                )
+            }
+        )
 
 
 class UserProfileForm(forms.ModelForm):