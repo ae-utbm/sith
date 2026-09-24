ae-utbm/sith3

force CGU/EULA approval on account creation

core/migrations/0051_user_eula_approved.py
@@ -1,0 +1,15 @@
+# Generated by Django 5.2.17 on 2026-09-15 11:02
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [("core", "0050_alter_sithfile_moderator")]
+
+    operations = [
+        migrations.AddField(
+            model_name="user",
+            name="eula_approved",
+            field=models.BooleanField(default=False, verbose_name="EULA approved"),
+        ),
+    ]

core/models.py
@@ -291,6 +291,7 @@ class User(AbstractUser):
         ),
         blank=True,
     )
+    eula_approved = models.BooleanField(_("EULA approved"), default=False)
     godfathers = models.ManyToManyField("User", related_name="godchildren", blank=True)
 
     objects = CustomUserManager()

ae-utbm/sith

core/migrations/0051_user_eula_approved.py
@@ -1,0 +1,15 @@
+# Generated by Django 5.2.17 on 2026-09-15 11:02
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [("core", "0050_alter_sithfile_moderator")]
+
+    operations = [
+        migrations.AddField(
+            model_name="user",
+            name="cgu_approved",
+            field=models.BooleanField(default=False, verbose_name="ToS approved"),
+        ),
+    ]

core/models.py
@@ -291,6 +291,7 @@ class User(AbstractUser):
         ),
         blank=True,
     )
+    cgu_approved = models.BooleanField(_("ToS approved"), default=False)
     godfathers = models.ManyToManyField("User", related_name="godchildren", blank=True)
 
     objects = CustomUserManager()